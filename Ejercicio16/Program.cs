36f28cd1b3c52cbf7b1ee82e6d5390a2df662b7f
GABRIEL GÓMEZ <g.gomez12@alumnos.santotomas.cl>
2021-04-14 23:42:31 -0400

    El ejercicio 16

---
+++ Ejercicio16/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             16-	Cree un programa de consola que solicite al cliente un número de minutos, el programa debe mostrar por pantalla a cuantas horas y minutos corresponden al número ingresado.
+             */
+
+                int minutos, horas = 0;
+
+                Console.WriteLine("Ingrese un número: ");
+                minutos = int.Parse(Console.ReadLine());
+
+                horas = minutos / 60;
+
+                Console.WriteLine("El número ingresado corresponde a: ");
+                Console.WriteLine($"En Horas: {horas}");
+                Console.WriteLine($"En Minutos: {minutos}");
+            
+                Console.ReadKey();
+        }
+    }
+}
