22597242a4451a8d113859da4a102729edd90e38
GABRIEL GÓMEZ <g.gomez12@alumnos.santotomas.cl>
2021-04-14 23:32:25 -0400

    El ejercicio 7

---
+++ Ejercicio7/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             7-	Cree un programa de consola que solicite dos números al cliente, el programa debe verificar que número es mayor y menor luego mostrar por pantalla todos los números desde el menor hasta el mayor de los números ingresados por el cliente.
+             */
+
+                Console.WriteLine("A continuación debe ingresar 2 números.");
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                if (n1 < n2)
+                {
+                    for (int n = n1; n <= n2; n++)
+                    {
+                        Console.WriteLine(n);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("El primer número debe ser menor al segundo.");
+                }
+            
+            Console.ReadKey();
+
+        }
+    }
+}
