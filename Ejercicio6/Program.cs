a41cf053a5118d4fb8610f930c8e6079bdc30917
GABRIEL GÓMEZ <g.gomez12@alumnos.santotomas.cl>
2021-04-14 23:29:16 -0400

    El ejercicio 6

---
+++ Ejercicio6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             6-	Cree un programa de consola que solicite un número al cliente y muestre por pantalla todos los números impares que hay desde el 1 hasta el número ingresado.
+             */
+
+                Console.WriteLine("Ingrese un número: ");
+                int numero = int.Parse(Console.ReadLine());
+
+                for (int n = 1; n <= numero; n++)
+                {
+                    if (n % 2 != 0)
+                    {
+                        Console.WriteLine(n);
+                    }
+                }
+            
+                Console.ReadKey();
+
+        }
+    }
+}
