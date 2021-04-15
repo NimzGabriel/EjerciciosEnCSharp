d1e209ecf89bcda1b9c432cefa1c0d142848dfc4
GABRIEL GÓMEZ <g.gomez12@alumnos.santotomas.cl>
2021-04-14 23:19:46 -0400

    Ejercicio 2

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             2-	Cree un programa de consola que solicite un número y muestre por pantalla si el número es par o impar, el 0 se considera par.
+             */
+
+            Console.WriteLine("Ingrese un número: ");
+            int num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0 || num == 0)
+            {
+                Console.WriteLine($"El número {num} es par.");
+            }
+            else
+            {
+                Console.WriteLine($"El número {num} es impar.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
