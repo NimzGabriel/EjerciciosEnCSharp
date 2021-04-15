79373f9cba475b41eab668b7881b7cd03b1df27e
GABRIEL GÓMEZ <g.gomez12@alumnos.santotomas.cl>
2021-04-14 23:25:31 -0400

    El ejercicio 1

---
+++ Ejercicion1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicion1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             1-	Cree un programa de consola que solicite una nota y muestre por pantalla si la nota es correcta o está fuera de rango. El rango correcto es de 1 a 7.
+             */
+
+            Console.WriteLine("Ingrese una nota: ");
+            int nota = int.Parse(Console.ReadLine());
+            if (nota >= 1 && nota <= 7)
+            {
+                Console.WriteLine("Nota Correcta!");
+            }
+            else
+            {
+                Console.WriteLine("Nota Fuera de Rango.");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
